107bac6371922c40f914d6a0179612bca202d108
Veakor <135655247+Veakor@users.noreply.github.com>
2024-02-09 17:05:22 +0100

    add

--- src/main.js
+++ src/main.js
@@ -125,7 +125,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
   loadMoreButton.id = 'loadMoreButton';
   loadMoreButton.textContent = 'Load more';
-  loadMoreButton.style.display = 'none'; /
+  loadMoreButton.style.display = 'none'; 
   
  
   loadMoreButton.addEventListener('click', async () => {
