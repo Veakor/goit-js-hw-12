171d0aed77aacbc074ac3b95044a8829363a6c87
Veakor <135655247+Veakor@users.noreply.github.com>
2024-02-14 15:59:33 +0100

    add

--- src/main.js
+++ src/main.js
@@ -99,9 +99,8 @@ document.addEventListener('DOMContentLoaded', async function(){
 
   const lightbox = new SimpleLightbox('.card-link');
   function displayImages(images) {
-    clearImages();
       const html = images.map(image => generateImageCard(image)).join('');
-      imageContainer.innerHTML += html; 
+      imageContainer.insertAdjacentHTML('beforeend', html);
       
       lightbox.refresh();
   }
